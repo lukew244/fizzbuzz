460caa7bc589730fe331c3ee8faaeeff9d2508ee
Luke <luke.webster86@gmail.com>
2016-06-28 11:33:09 +0000

    Working fizzbuzz and tests in rspec

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,14 @@
 def fizzbuzz(integer)
-  "fizz"
+
+
+  if integer % 15 == 0
+    "fizzbuzz"
+  elsif integer % 3 == 0
+    "fizz"
+  elsif integer % 5 == 0
+    "buzz"
+  else 
+    integer
+  end
+
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,20 @@
 require 'fizzbuzz'
 
 describe "fizzbuzz" do
+  it "fizzbuzz" do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+
   it "fizz" do
     expect(fizzbuzz(3)).to eq "fizz"
   end
+
+  it "buzz" do
+    expect(fizzbuzz(5)).to eq "buzz"
+  end
+
+  it "return integer" do
+    expect(fizzbuzz(2)).to eq 2
+  end
+
 end
